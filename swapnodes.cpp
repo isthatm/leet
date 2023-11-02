a040662ade5d14e3363004b73fdfdcd3120150f8
minhle <fixed-term.Minh.Le3@vn.bosch.com>
2023-11-02 10:13:38 +0700

    done

--- swapnodes.cpp
+++ swapnodes.cpp
@@ -4,9 +4,9 @@ using namespace std;
 struct Node {
     int val;
     Node* next; 
-    Node() : val(0), next(nullptr) {cout << "default initialization" << endl;} //default constructor, used when creating new nodes
+    Node() : val(0), next(nullptr) {}
     Node(int x) : val(x), next(nullptr) {} 
-    Node(int x, Node* next) : val(x), next(next) {} //next(next_arg)
+    Node(int x, Node* next) : val(x), next(next) {} 
 };
 
 class ListNodes{
@@ -22,7 +22,7 @@ class ListNodes{
             delete current;
             while (true){
                 current = temp;
-                if (current->next == NULL){
+                if (current == NULL || current->next == NULL){
                     break;
                 }
                 temp = current->next;
@@ -37,7 +37,6 @@ class ListNodes{
             } 
             Node* temp = head; //starts at head, temp and head now point to the same address
             while (temp->next != NULL){
-                // for the first loop (from the 2nd node), temp->next = NULL, this loop is skipped
                 temp = temp->next; //dereferencing the pointer stored in next
             } 
             temp->next = new_node; // update indirectly through pointer
@@ -70,45 +69,49 @@ class ListNodes{
                 return current_node;
            }
            current_node = swapConsecutive(this->head);
-           do{
-                temp = current_node->next;
-                current_node->next = temp->next;
-                current_node = temp;
-                current_node = swapConsecutive(current_node);
-                // cout << current_node->val << endl;
-           }while(current_node->next != NULL);
-
+           if(current_node->next != NULL) {
+                if(current_node->next->next != NULL){
+                    while(true){
+                        temp = current_node->next;
+                        current_node->next = temp->next;
+                        current_node = swapConsecutive(temp);
+                        if(
+                            current_node->next == NULL ||
+                            current_node->next->next == NULL
+                            ){
+                            break;
+                        }
+                    }
+                }
+           }
            return this->head;
         }
+
     private:
         Node* swapConsecutive(Node* current_node){// current_node is either head or (tail -2)
         // 1 and 2 is swapped but two is not printed because head is still pointing to Node 1
         // NOTE: call this when '1 2 3 N or '1 2 N
-            // cout << "head is: " << head << ", " << "current node is: " << current_node << endl; 
-            // cout << "before next next: " << current_node->next->next << endl;
             if (current_node == head){
                 head = current_node->next;
             }
             Node* temp = current_node->next;
             current_node->next = temp->next;
             temp->next = current_node;
-            // cout << "after next: " << current_node->next << ", head is: " << head << endl;
-
             return current_node; // address of the Node next to current node after they're swapped 
         }
 };
 
 int main(){
     ListNodes list;   
-    Node* main_node;
-    list.insertNode(1);
-    list.insertNode(2);
-    list.insertNode(3);
-    list.insertNode(4);
+    Node* input_node;
+    // list.insertNode(1);
+    // list.insertNode(2);
+    // list.insertNode(3);
+    // list.insertNode(4);
     // list.insertNode(5);
     // list.insertNode(6);
-    list.swapPairs(list.head);
-    // main_node = list.swapHeadTail(list.head);
-    // cout << "res: " << main_node->next << endl;
-    // list.printList();
+    // list.insertNode(7);
+    // input_node = new Node();
+    list.swapPairs(input_node);
+    list.printList();
 }
