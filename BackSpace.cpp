ea51ad9cfd350bc94389b7aaa9468ed90035e26b
minhle <fixed-term.Minh.Le3@vn.bosch.com>
2023-11-06 15:03:55 +0700

    almost worked backspace

--- BackSpace.cpp
+++ BackSpace.cpp
@@ -1,35 +1,61 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-		string res_s = backspaceHandler(s);
-		string res_t = backspaceHandler(t);
+		int s_ptr = s.length() - 1;
+		int t_ptr = t.length() - 1;
 
-		return res_s == res_t;
+		while (s_ptr >= 0 && t_ptr >= 0){
+			
+			s_ptr = backspace(s, s_ptr);
+			t_ptr = backspace(t, t_ptr);
+			// s[s_ptr] and t[t_ptr] can only fall into either 1 of these twos:
+			// - negative value
+			// - a valid char
+			if (s_ptr >= 0 && t_ptr >= 0){
+				if (s[s_ptr] != t[t_ptr]){
+					return false;
+				}
+			}
+
+			s_ptr--;
+			t_ptr--;
+		}
+		
+		if (s_ptr < 0 && t_ptr < 0){
+			cout << "trueeee" << endl;
+			return true;
+		} else{
+			return false;
+		}
     }
 private:
-	string backspaceHandler(string s){
-		vector<char> v_string(s.begin(), s.end());
-		for(int i=0; i < v_string.size(); i++){
-			if (v_string[i] == '#'){
-				v_string.erase(v_string.begin() + i);
-				v_string.erase(v_string.begin() + i - 1);
-				i = 0;
-				continue;
+	int backspace(string str, int ptr){
+		int backspace = 0;
+	
+		while(ptr >= 0){
+			if(str[ptr] == '#'){
+				backspace++;
+			} else if (backspace > 0){
+			// not # and backspace > 0 -> char to be deleted
+			// backspace is also deleted
+				backspace--;
+			} else{
+				//exit the loop at the valid char
+				break;
 			}
+			ptr--;		
 		}
-		string res_str(v_string.begin(), v_string.end());
-		
-		return res_str;
+		cout << str << "," << ptr << endl;
+		return ptr; 
 	}
 };
 
 int main(){
-	string s = "a##c";
-	string t = "#a#c";
+	string s = "bxj##tw";
+	string t = "bxj###tw";
 	Solution sol;
 	bool res = sol.backspaceCompare(s, t);
 	cout << res << endl;
