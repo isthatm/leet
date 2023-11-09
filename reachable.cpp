19d2dfceb0b655a35feff18ce11b09a88a0357d4
minhle <fixed-term.Minh.Le3@vn.bosch.com>
2023-11-09 15:15:29 +0700

    reachable 799/805

--- reachable.cpp
+++ reachable.cpp
@@ -4,15 +4,27 @@ using namespace std;
 class Solution {
 public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        int chebyshev_dis = max(abs(sx - sy), abs(fx - fy));
+        int chebyshev_dis = max(abs(sx - fx), abs(sy - fy));
         if (
             (sx == fx) && (sy == fy)
         ){
-            return false;
-        } else {
-            t >= chebyshev_dis;
+            if (t % 2 == 0){
+                return true;
+            } else{
+                return false;
+            }
+        }
+        cout << "min_dist = " << chebyshev_dis << endl; 
+        if (t >= chebyshev_dis){
             return true;
+        } else{
+            return false;
         }
     }
 };
 
+int main(){
+    Solution sol;
+    bool res = sol.isReachableAtTime(1, 1, 1, 1, 3);
+    cout << res << endl;
+}
