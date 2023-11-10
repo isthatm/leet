3a25c434aa0785c5647cd513a12da9c912c5e45f
minhle <fixed-term.Minh.Le3@vn.bosch.com>
2023-11-10 16:04:12 +0700

    HashMap demo

---
+++ HashMap/KeyHash.h
@@ -0,0 +1,8 @@
+int TABLE_SIZE = 10;
+template <typename K>
+struct KeyHash {
+    unsigned long operator()(const K& key) const
+    {
+        return reinterpret_cast<unsigned long>(key) % TABLE_SIZE;
+    }
+};
---
+++ HashMap/test.cpp
@@ -0,0 +1,19 @@
+#include "HashMap.h"
+
+int main(){
+    HashMap<int, string, MyKeyHash> hmap;
+    hmap.put(1, "val1");
+    hmap.put(2, "val2");
+    hmap.put(3, "val3");
+
+    string value;
+    hmap.get(2, value);
+    cout << value << endl;
+    bool res = hmap.get(3, value);
+    if (res)
+        cout << value << endl;
+    hmap.remove(3);
+    res = hmap.get(3, value);
+    if (res)
+    cout << value << endl;
+}
