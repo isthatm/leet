565b8751916a69207289b841dfe3b95964d7c688
Le Minh <minhle.eer@gmail.com>
2023-11-18 22:03:09 +0700

    working on test case 42 int min

--- atoi.cpp
+++ atoi.cpp
@@ -21,30 +21,34 @@ public:
             if (!hash_map.get(ASCII_key, value)){
                 if (!valid_counter){
                     switch (ASCII_key){
-                    case 32: // "space"
-                        i++;
-                        continue;
-                    case 43: // "+"
-                        i++;
-                        continue; 
-                    case 45: // "-"
-                            is_minus = true;
+                        case 32: // "space"
+                            i++;
+                            continue;
+                        case 43: // "+"
                             i++;
                             continue; 
-                    default:
-                        break;
+                        case 45: // "-"
+                                is_minus = true;
+                                i++;
+                                continue; 
+                        default:
+                            break;
                     } 
-                } else { break; }
+                } 
+                break; 
             } else {
                 valid_counter++;
                 bool available = hash_map.get(ASCII_key, value);
                 res = parseNums(res, value);
+                if (res == INT32_MAX){
+                    break;
+                }
             }
         i++;
         }
 
         if (is_minus) {return -res;}
-        return -res;
+        return res;
     }
 
 private:
@@ -52,18 +56,49 @@ private:
     int value;
 
     int parseNums(int prev_num, int num){ 
-        return ((prev_num*10) + num);
+        try{
+            if (prev_num != 0 && INT32_MAX / prev_num < 10){
+                throw range_error("INT32 overflow will occur");
+            }
+        }
+        catch (range_error err){
+            return INT32_MAX;
+        }
+        int res = (prev_num*10) + num; 
+        return res;
     }
 };
 
 int main(){ 
-    string s = " -1010123lol";
+    string s = "-91283472332";
     Solution sol;
     int res = sol.myAtoi(s);
     cout << res << endl;
-    // const char* str = " -1010123lol";
+    // const char* str = "-91283472332";
     // cout << atoi(str) << endl;
     // char a = '5';
     // char* ptr = &a;
     // cout << hash_function(ptr) << endl;
 }   
+
+// int main()
+// {
+//    int x = -1;
+ 
+//    // Some code
+//    cout << "Before try \n";
+//    try {
+//       cout << "Inside try \n";
+//       if (x < 0)
+//       {
+//          throw x;
+//          cout << "After throw (Never executed) \n";
+//       }
+//    }
+//    catch (int x ) {
+//       cout << "Exception Caught: " << x << endl;
+//    }
+ 
+//    cout << "After catch (Will be executed) \n";
+//    return 0;
+// }
--- minh_hash/HashFun.h
+++ minh_hash/HashFun.h
@@ -1,4 +1,4 @@
-int TABLE_SIZE = 10; // this is equal to the number of buckets
+const int TABLE_SIZE = 10; // this is equal to the number of buckets
 // one bucket can contain many keys EX: bucket 1 can contain key 1 and 11 
 template <typename K>
 class HashFun{
