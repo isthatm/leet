53be68f924c77ef193779629bdd81baf73c4e9ef
Le Minh <minhle.eer@gmail.com>
2023-11-19 13:40:02 +0700

    fone atoi.cpp

--- atoi.cpp
+++ atoi.cpp
@@ -61,26 +61,26 @@ private:
 
     int parseNums(int prev_num, int num){ 
         try{
-            if (prev_num < 0){
-                if (prev_num == -1){ } 
-                else if (INT32_MIN / prev_num < 10){
-                    throw INT32_MIN;
-                }
+            // Multiplication check
+            if (prev_num == -1){ } 
+            else if (prev_num < 0 && INT32_MIN / prev_num < 10){
+                throw INT32_MIN;
+            }
 
-                if (prev_num < INT32_MIN - num){
-                    throw INT32_MIN;
-                }
-            }
-            else if (prev_num > 0){
-                if (INT32_MAX / prev_num < 10){
+            if (prev_num > 0 && INT32_MAX / prev_num < 10){
                     throw INT32_MAX;
-                } 
-                
-                if (prev_num > INT32_MAX - num){
-                    throw INT32_MAX;
-                }
+            } 
+            prev_num = prev_num * 10;
+
+            // Addition check
+            if (prev_num < 0 && prev_num < INT32_MIN - num){
+                throw INT32_MIN;
             }
-            int res = (prev_num*10) + num; 
+            if (prev_num > 0 && prev_num > INT32_MAX - num){
+                throw INT32_MAX;
+            }
+         
+            int res = prev_num + num; 
             return res;
         }
         catch (int clamped_res){
@@ -94,31 +94,4 @@ int main(){
     Solution sol;
     int res = sol.myAtoi(s);
     cout << res << endl;
-    // const char* str = "-91283472332";
-    // cout << atoi(str) << endl;
-    // char a = '5';
-    // char* ptr = &a;
-    // cout << hash_function(ptr) << endl;
 }   
-
-// int main()
-// {
-//    int x = -1;
- 
-//    // Some code
-//    cout << "Before try \n";
-//    try {
-//       cout << "Inside try \n";
-//       if (x < 0)
-//       {
-//          throw x;
-//          cout << "After throw (Never executed) \n";
-//       }
-//    }
-//    catch (int x ) {
-//       cout << "Exception Caught: " << x << endl;
-//    }
- 
-//    cout << "After catch (Will be executed) \n";
-//    return 0;
-// }
