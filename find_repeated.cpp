5921ebdd50b55cdb228b5b84773614c33f3af5ac
minhle <fixed-term.Minh.Le3@vn.bosch.com>
2023-11-28 16:58:26 +0700

    reformat binary search

--- find_repeated.cpp
+++ find_repeated.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 int repeatedNumber(int arr[], size_t arr_length){
     int start_idx = 0;
     int end_idx = arr_length;
@@ -9,7 +10,6 @@ int repeatedNumber(int arr[], size_t arr_length){
     while (arr[mid_idx] > mid_idx){
         start_idx = mid_idx;
         mid_idx = ((end_idx - start_idx) / 2) + start_idx;
-
     }
 
     // Repeated number is on the left
@@ -17,16 +17,13 @@ int repeatedNumber(int arr[], size_t arr_length){
     {
         end_idx = mid_idx;
         mid_idx = end_idx  / 2;
-        if (mid_idx == 2){
-            break;
-        }
     }
     
     return arr[mid_idx];
 }
 
 int main(){
-    int arr[] = {1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10, 11};
+    int arr[] = {1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     int res = repeatedNumber(arr, sizeof(arr) / sizeof(int));
     cout << res << endl;
 }
