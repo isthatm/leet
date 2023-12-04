70cdd31686c30cd0700b899f7957111a3f95ed51
minhle <fixed-term.Minh.Le3@vn.bosch.com>
2023-12-04 17:33:49 +0700

    Work on the overflow

--- reverseInt.cpp
+++ reverseInt.cpp
@@ -6,17 +6,19 @@ class Solution {
 public:
     int reverse(int x) {
         int temp = x;
+        x = 0;
         while (temp != 0) {
+            x = (x + temp % 10)*10;
             temp /= 10;
-            x = (temp % 10) + temp*10;
         }
-        
+        return x / 10;
     }
 };
 
 int main() {
     Solution sol;
-    int x = 123;
+    int x = -123;
     int res = sol.reverse(x);
     cout << "Inversed x = " << res << endl;
+    // cout << 123 / 10 << endl;
 }
