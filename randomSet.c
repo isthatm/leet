407fa852b93dde0deaca1a1b00487fcf58ce3558
Le Minh <minhle.eer@gmail.com>
2023-12-24 16:53:44 +0700

    Establishing the backbone

--- randomSet.c
+++ randomSet.c
@@ -1,37 +1,57 @@
 /*
     Problem 380. Insert Delete GetRandom O(1)
+    In this problem the value is the key and vice versa
 */
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+
+#define TABLE_SIZE 127
 
 typedef struct {
-    
-} RandomizedSet;
-
-typedef struct {
-    int value;
-    Node* next;
+    unsigned value;
+    int hash;
+    struct Node* next;
 } Node;
 
+typedef struct {
+    Node **buckets;
+} RandomizedSet;
 
-RandomizedSet* randomizedSetCreate() {
+int hash_fun(int key) {
+    unsigned u_key  = (unsigned) key;
+    u_key = ((u_key >> 16) ^ u_key) * 0x45d9f3b;
+    u_key = ((u_key >> 16) ^ u_key) * 0x45d9f3b;
+    u_key = (u_key >> 16) ^ u_key;
+    return u_key % TABLE_SIZE;
+}
+
+RandomizedSet *randomizedSetCreate() {
+    RandomizedSet *map_t;
+    map_t->buckets = (Node **)calloc(TABLE_SIZE, sizeof(Node *));
+    return map_t;
+}
+
+bool randomizedSetInsert(RandomizedSet *obj, int val) {
     
 }
 
-bool randomizedSetInsert(RandomizedSet* obj, int val) {
+bool randomizedSetRemove(RandomizedSet *obj, int val) {
     
 }
 
-bool randomizedSetRemove(RandomizedSet* obj, int val) {
+int randomizedSetGetRandom(RandomizedSet *obj) {
     
 }
 
-int randomizedSetGetRandom(RandomizedSet* obj) {
-    
+void randomizedSetFree(RandomizedSet *obj) {
+    // TODO: Free the nodes on the linked lists also
+    free(obj);
 }
 
-void randomizedSetFree(RandomizedSet* obj) {
-    
+int main() {
+    RandomizedSet* obj = randomizedSetCreate();
+    randomizedSetFree(obj);
 }
 
 /**
